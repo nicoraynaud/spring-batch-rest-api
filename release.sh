3921d70920b552a01efb5d8daae563a8f5e921dd
Nicolas Raynaud <nicolas@hardbacon.com>
2019-04-16 17:08:54 -0400

    Release version 0.1.0

--- release.sh
+++ release.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-sed -i 's/^version=.*/version='"$1"'/g' gradle.properties
+if [ "$1" == "" ] ||  [ "$2" == "" ]; then
+    echo "Should pass the versions in parameter"
+    exit 1
+fi
+
+sed -i '' "s/^version=.*/version=$1/g" gradle.properties
 
 gradle clean build --no-daemon --refresh-dependencies
 
@@ -8,10 +13,10 @@ gradle createPom --no-daemon
 
 git commit -a -m "Release version $1"
 git tag -a "v$1" -m "Release version $1"
-git push origin HEAD:master
-git push origin "v$1"
+#git push origin HEAD:master
+#git push origin "v$1"
 
-sed -i 's/^version=.*/version='"$2"'/g' gradle.properties
+sed -i '' "s/^version=.*/version=$2/g" gradle.properties
 git commit -a -m "New SNAPSHOT version $2"
-git push origin HEAD:master
-git push origin "v$2"
+#git push origin HEAD:master
+#git push origin "v$2"
