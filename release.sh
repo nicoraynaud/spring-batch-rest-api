548ec337bce3c0004558cf2e658a99b0a800f77f
Nicolas Raynaud <nicolas@hardbacon.com>
2021-11-17 11:14:35 -0500

    adding jitpack.yml to specify openjdk11 build

--- release.sh
+++ release.sh
@@ -17,4 +17,4 @@ git tag -a "v$1" -m "Release version $1"
 sed -i '' "s/^version=.*/version=$2/g" gradle.properties
 git commit -a -m "New SNAPSHOT version $2"
 git push origin HEAD:master
-git push origin "v$1"
+git push origin --tags
